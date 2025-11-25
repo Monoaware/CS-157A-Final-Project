a9d388dfa891106b33c92935f8681ee6d9dd00c3
ramenalmond <raymondwsomething@gmail.com>
2025-11-25 14:19:10 -0800

    Refactored db initialize to run the new db files, also added new db files, will implement them after

---
+++ backend/app/src/main/java/edu/sjsu/library/utils/DatabaseInitializer.java
@@ -0,0 +1,32 @@
+package edu.sjsu.library.utils;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+@Configuration
+public class DatabaseInitializer implements CommandLineRunner {
+
+    private final DataSource dataSource;
+
+    public DatabaseInitializer(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        try (Connection conn = dataSource.getConnection()) {
+            // Run schema SQL
+            ScriptUtils.executeSqlScript(conn, new ClassPathResource("db/create_schema.sql"));
+
+            // Run data initialization SQL
+            ScriptUtils.executeSqlScript(conn, new ClassPathResource("db/initialize_data.sql"));
+
+            System.out.println("Database initialized successfully!");
+        }
+    }
+}
